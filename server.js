f51137c1a44c36f491769547c74b526d622bdcd5
Anna <annamary.alava@gmail.com>
2021-03-23 17:43:22 +0200

    update

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/cat", require("./routes/catRoutes.js"));
+
 db.on("connected", () => {
   app.listen(3000, () => {
     console.log("express server started port 3000");
