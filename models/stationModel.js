2096fbe074ea39c617c54c8f37c4060658bfd19b
Anna <annamary.alava@gmail.com>
2021-03-26 18:44:51 +0200

    update

---
+++ models/stationModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const stationSchema = new Schema({
+  Title: String,
+  Town: String,
+  AddressLine1: String,
+  StateOrProvince: String,
+  Postcode: String,
+  Location: {
+    type: {
+      type: String,
+      enum: ["Polygon"],
+      required: true,
+    },
+    coordinates: {
+      type: [[[Number]]],
+      required: true,
+    },
+  },
+});
+module.exports = mongoose.model("Station", stationSchema);
