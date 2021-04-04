6f0c61428c6ea7ea8ecd6f4900fe220970c13eb1
Anna <annamary.alava@gmail.com>
2021-04-04 15:32:54 +0300

    update

--- db.js
+++ db.js
@@ -15,22 +15,3 @@ const connectMongo = async () => {
 };
 
 export default connectMongo;
-
-/*
-const mongoose = require("mongoose");
-
-// Currently connects to chargemap db
-(async () => {
-  try {
-    await mongoose.connect(process.env.CM_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("DB connected successfully");
-  } catch (err) {
-    console.error("Connection to db failed", err);
-  }
-})();
-
-module.exports = mongoose.connection;
-*/
--- server.js
+++ server.js
@@ -32,27 +32,3 @@ dotenv.config();
     console.log("server error: " + e.message);
   }
 })();
-
-/*
-"use strict";
-
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const db = require("./db");
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/cat", require("./routes/catRoutes.js"));
-app.use("/station", require("./routes/stationRoutes.js"));
-app.use("/connections", require("./routes/connectionRoutes.js"));
-//app.use("/connectiontype", require("./routes/connectionTypeRoutes.js"));
-app.use("/currenttype", require("./routes/currentTypeRoute.js"));
-app.use("/level", require("./routes/levelRoutes.js"));
-
-db.on("connected", () => {
-  app.listen(3000, () => {
-    console.log("express server started port 3000");
-  });
-});
-*/
