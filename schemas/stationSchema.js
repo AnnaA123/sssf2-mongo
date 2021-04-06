fd39afab08f7c695ca9a1fed2ad2238162be0b64
Anna <annamary.alava@gmail.com>
2021-04-06 09:23:28 +0300

    update

--- schemas/stationSchema.js
+++ schemas/stationSchema.js
@@ -2,8 +2,8 @@ import { gql } from "apollo-server-express";
 
 export default gql`
     extend type Query {
-        stations: [Station]
-        station(id: ID!): Station
+        station(id: ID): Station
+        stations(bounds: Bounds, limit: Int, start: Int): [Station] 
     }
 
     type Station {
@@ -17,5 +17,30 @@ export default gql`
             coordinates: [Number]
         }
         Connections: Connection
+    }
+
+    extend type Mutation {
+        addStation(
+            Connections: [ConnectionInput]
+            Title: String!
+            AddressLine1: String
+            Town: String
+            StateOrProvince: String
+            Postcode: String
+            Location: PointObjectInput
+        ): Station
+
+        modifyStation(
+            id: ID!
+            Connections: [ConnectionInput]
+            Title: String
+            AddressLine1: String
+            Town: String
+            StateOrProvince: String
+            Postcode: String
+            Location: PointObjectInput
+        ): Station
+
+        deleteStation(id: ID!): Station
     }
 `;
