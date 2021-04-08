b2bce03630820efbdd0dec7bca695747d336fc6e
Anna <annamary.alava@gmail.com>
2021-04-08 19:13:44 +0300

    update

--- resolvers/index.js
+++ resolvers/index.js
@@ -3,19 +3,8 @@ import connectionResolver from "./connectionResolver.js";
 import connectionTypeResolver from "./connectionTypeResolver.js";
 import currentTypeResolver from "./currentTypeResolver.js";
 import levelResolver from "./levelResolver.js";
-import { gql } from "apollo-server-express";
-
-const linkSchema = gql`
-  type Query {
-    _: Boolean
-  }
-  type Mutation {
-    _: Boolean
-  }
-`;
 
 export default [
-  linkSchema,
   stationResolver,
   connectionResolver,
   connectionTypeResolver,
