47d98919b3f674b6523a58baa64950c1c49339b0
Anna <annamary.alava@gmail.com>
2021-04-09 16:40:10 +0300

    update

--- routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import { express } from "express";
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController.js");
-
-router.post("/login", authController.login);
-
-module.exports = router;
