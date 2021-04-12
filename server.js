3195e633ec9ec4cd57f727e715930ad6cd86a6d7
Anna <annamary.alava@gmail.com>
2021-04-12 09:15:31 +0300

    update

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const db = require("./db");
-
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/cat", require("./routes/catRoutes.js"));
-app.use("/station", require("./routes/stationRoutes.js"));
-app.use("/connections", require("./routes/connectionRoutes.js"));
-app.use("/connectiontype", require("./routes/connectionTypeRoutes.js"));
-app.use("/currenttype", require("./routes/currentTypeRoute.js"));
-app.use("/level", require("./routes/levelRoutes.js"));
-
-db.on("connected", () => {
-  app.listen(3000, () => {
-    console.log("express server started port 3000");
-  });
-});
-=======
 import { ApolloServer } from "apollo-server-express";
 import schemas from "./schemas/index.js";
 import resolvers from "./resolvers/index.js";
@@ -105,4 +82,3 @@ const options = {
     console.log("server error: " + e.message);
   }
 })();
->>>>>>> gql-auth
