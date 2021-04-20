cfe05add4fe9feedebb02078c66f781e9fe02e3a
Anna <annamary.alava@gmail.com>
2021-04-20 12:11:01 +0300

    update

--- server.js
+++ server.js
@@ -60,8 +60,8 @@ const options = {
     if (process.env.NODE_ENV === "production") {
       production(app, 3000);
     } else {
-      // production(app, 3000);
-      localhost(app, 8000, 3000);
+      production(app, 3000);
+      // localhost(app, 8000, 3000);
     }
 
     /*
